e010030feffbae657305b7bcf2c675dc7a44f70f
Tim Benedikt Herbstrith <tim.benedikt.herbstrith@univie.ac.at>
2016-06-16 10:08:54 +0200

    Automatically update ALSA-input device before starting Darkice

--- lmsio.py
+++ lmsio.py
@@ -4,6 +4,8 @@
 from pylms.server import Server
 import socket
 import time
+import re
+import os
 import subprocess
 
 def connect_to_player_at_server(playername, hostname):
@@ -24,7 +26,26 @@ def connect_to_player_at_server(playername, hostname):
 
 def phono(p=None):
     if p == None:
-        p = subprocess.Popen(['sudo', 'darkice'])
+        arecord = subprocess.Popen(['arecord', '-l'], stdout=subprocess.PIPE)
+        hw = arecord.communicate()[0]
+	print hw
+        try:
+            pat_dev = r'card (\d+):'
+            device = re.search(pat_dev, hw).groups(1)[0]
+            pat_sub = r'Subdevice #(\d+):'
+            subdev = re.search(pat_sub, hw).groups(1)[0]
+        except AttributeError:
+            return
+        path = os.path.split(os.path.realpath(__file__))[0]
+        cfg_in_fn = os.path.join(path, 'darkice.cfg')
+	cfg_out_fn = os.path.join(path, '.cfg')
+
+        with open(cfg_in_fn, 'r') as cfg_in:
+            cfg = cfg_in.read()
+        with open(cfg_out_fn, 'w') as cfg_out:
+            cfg_out.write(cfg.format(dev=device, sub=subdev))
+
+        p = subprocess.Popen(['sudo', 'darkice', '-c', cfg_out_fn])
         return p
     else:
         p.kill()
