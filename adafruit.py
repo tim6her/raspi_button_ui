394b0cd351259d38ebd9cea5f1e59a7ca1ad8b46
Tim B. Herbsrith <tim.herbstrith@icloud.com>
2017-09-30 20:37:21 +0200

    Using pushbullet directely

--- adafruit.py
+++ adafruit.py
@@ -4,6 +4,7 @@ import os
 import time
 
 import Adafruit_IO as aio
+import pushbullet
 
 import cartridge
 
@@ -11,8 +12,11 @@ def main():
     path = os.path.split(os.path.realpath(__file__))[0]
     log_fn = os.path.join(path, 'cartridge.log')
     ada_fn = os.path.join(path, 'adafruit.txt')
+    pb_fn = os.path.join(path, 'pushbullet.txt')
     with open(ada_fn, 'r') as ada:
         client = aio.Client(ada.read().strip('\n'))
+    with open(pb_fn, 'r') as pbf:
+        pb = pushbullet.Pushbullet(pbf.read().strip('\n'))
 
     old_id = None
     while True:
@@ -31,9 +35,11 @@ def main():
         if old_id != new_id:
             usage = cartridge.usage_list(log_fn, realtime=True)
             use = cartridge.usetime(usage)
-            client.send('cartridge', '%.2f' % (use / 60. / 60.))
+            title = 'CARTRIDE USAGE'
+            body = 'Your cartridge operated %.2f hours.' % (use / 60. / 60.)
+            pb.push_note(title, body)
 
         old_id = new_id
 
-if __name__ == "main":
+if __name__ == "__main__":
     main()
